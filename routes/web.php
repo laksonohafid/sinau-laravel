9662c2a2e6519c95c0b1f7aacd5fdc40f3274b7d
laksonohafid <lksn.hafid32@gmail.com>
2021-01-27 13:09:32 +0700

    Mengganti Tampilan, edit dan delete data

--- routes/web.php
+++ routes/web.php
@@ -2,77 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('beranda');
+// Home
+Route::get('/', function() {
+	return view('home');
 });
 
-Route::get('/member', function () {
-    return view('index');
+// About
+Route::get('/about', function() {
+	return view('about');
 });
 
+// Data
+Route::resource('/datasiswa', 'siswaController');
 
-//Route crud
-Route::get('/member','MemberController@index');
-Route::get('/tambah', 'MemberController@create');
-Route::post('/member', 'MemberController@store');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// // closure
-// Route::get('/profil', function(){
-//     return "ini adalah profile siswa";
-// })->name('profil');
-
-// // Route name
-// Route::get('/tesname', function(){
-//     return route('profil');
-// });
-
-// // Parameter
-// Route::get('/profil/{id}', function($id){
-//     return $id;
-// });
-
-// // Controller
-// Route::get("/tescontroller", "TesController@show");
-
-// // Resource
-// Route::resource('siswa', "SiswaController");
+//edit update dan delete data
+Route::get('/data/{id}/edit', 'SiswaController@edit')->name('editData');
+Route::post('/data/{id}/update', 'SiswaController@update')->name('updateData');
+Route::get('/data/{id}/delete', 'SiswaController@destroy')->name('deleteData');
